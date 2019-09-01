f0231d76cb991002d48089c480123b2b3d8e670c
Chris Luiz <chris@chrisluiz.io>
2019-09-01 12:57:53 -0400

    adding utils file

---
+++ utils.py
@@ -0,0 +1,9 @@
+
+def read_config(config_file):
+    # read file ansd split on newlines
+    with open(config_file, 'r') as f:
+        val_list = f.read().split('\n')
+    # split on = sign
+    split_vals = [x.split('=') for x in val_list]
+    # return dict of values. Check length to prevent error on malformed entries
+    return {item[0]: item[1] for item in split_vals if len(item) == 2}
