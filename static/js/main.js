a099647c5d284051c92c2c282057da2a2b97833d
Chris Luiz <chris@chrisluiz.io>
2019-09-16 06:58:25 -0400

    ajax for data refresh stubbed out

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,32 @@
+// Data table constructor
 $(function() {
   $('#dataTable').DataTable();
 });
+
+// Event handlers
+$(function() {
+  $("#updateData").click(getAllData(), window.reload())
+});
+
+// Include the most common AJAX settings for clarity
+function getAllData() {
+  $.ajax({
+    url: '/refresh_data/',
+    beforeSend: function () {
+      // Not yet implemented
+      // updateModal.show(),
+      // glyphicon.spin()
+    },
+    success:function () {
+      //pass
+    },
+    error: function () {
+      //pass
+    },
+    complete: function () {
+      //pass
+    },
+  })
+}
+
 
